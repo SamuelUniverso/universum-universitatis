a49647b899216f10f8a1f91732788f8c4a29ab3b
SamuelUniverso <samuel.rockenbach@universo.univates.br>
2022-05-24 01:05:27 -0300

    Update app.database.db.sql

--- src/main/resources/database/app.database.db.sql
+++ src/main/resources/database/app.database.db.sql
@@ -1,4 +1,5 @@
 BEGIN TRANSACTION;
+
 CREATE TABLE IF NOT EXISTS "pessoas" (
 	"id"	INTEGER NOT NULL UNIQUE,
 	"prenome"	TEXT NOT NULL,
@@ -8,26 +9,37 @@ CREATE TABLE IF NOT EXISTS "pessoas" (
 	"cpf"	TEXT NOT NULL UNIQUE,
 	PRIMARY KEY("id")
 );
+
+CREATE TABLE IF NOT EXISTS "ldap" (
+	  "usuario" TYPE text
+	, "senha" TYPE text
+	, "fk_pessoa" TYPE integer REFERENCES "pessoas"("id")
+);
+
 CREATE TABLE IF NOT EXISTS "alunos" (
 	"fk_pessoa"	INTEGER UNIQUE,
 	"codigo_matricula"	INTEGER NOT NULL,
 	FOREIGN KEY("fk_pessoa") REFERENCES "pessoas"("id")
 );
+
 CREATE TABLE IF NOT EXISTS "professores" (
 	"fk_pessoa"	INTEGER NOT NULL UNIQUE,
 	"codigo_matricula"	INTEGER NOT NULL,	
 	FOREIGN KEY("fk_pessoa") REFERENCES "pessoas"("id")
 );
+
 CREATE TABLE IF NOT EXISTS "cursos" (
 	"id"	INTEGER NOT NULL UNIQUE,
 	"nome"	INTEGER NOT NULL,
 	PRIMARY KEY("id")
 );
+
 CREATE TABLE IF NOT EXISTS "disciplinas" (
 	"id"	INTEGER NOT NULL UNIQUE,
 	"nome"	TEXT NOT NULL,
 	PRIMARY KEY("id")
 );
+
 CREATE TABLE IF NOT EXISTS "contratos" (
 	"id"	INTEGER NOT NULL UNIQUE,
 	"fk_aluno"	INTEGER NOT NULL,
@@ -38,11 +50,13 @@ CREATE TABLE IF NOT EXISTS "contratos" (
 	FOREIGN KEY("fk_curso") REFERENCES "cursos"("id"),
 	FOREIGN KEY("fk_aluno") REFERENCES "alunos"("id")
 );
+
 CREATE TABLE IF NOT EXISTS "periodos" (
 	"id"	INTEGER NOT NULL UNIQUE,
 	"periodo_letivo"	TEXT NOT NULL,
 	PRIMARY KEY("id")
 );
+
 CREATE TABLE IF NOT EXISTS "turmas" (
 	"id"	INTEGER NOT NULL UNIQUE,
 	"fk_professor"	INTEGER NOT NULL,
@@ -53,6 +67,7 @@ CREATE TABLE IF NOT EXISTS "turmas" (
 	FOREIGN KEY("fk_periodo") REFERENCES "periodos"("id"),
 	FOREIGN KEY("fk_professor") REFERENCES "professores"("id")
 );
+
 CREATE TABLE IF NOT EXISTS "matriculas" (
 	"id"	INTEGER NOT NULL UNIQUE,
 	"fk_contrato"	INTEGER NOT NULL,
@@ -67,4 +82,17 @@ CREATE TABLE IF NOT EXISTS "matriculas" (
 	FOREIGN KEY("fk_turma") REFERENCES "turmas"("id"),
 	FOREIGN KEY("fk_contrato") REFERENCES "contratos"("id")
 );
+
+-- Dummy Data
+INSERT INTO ldap (usuario, senha,fk_pessoa) 
+	VALUES ('admin','21232f297a57a5a743894a0e4a801fc3', null);
+
+INSERT INTO "pessoas"(id, prenome, nome, sobrenome, telefone, cpf) 
+	VALUES (1, 'Nobilis', 'Discipullum', 'Honorarium', '5551987654321', '22177716683');
+INSERT INTO "alunos"(fk_pessoa, codigo_matricula) VALUES (1, '221-777-166');
+
+INSERT INTO "pessoas"(id, prenome, nome, sobrenome, telefone, cpf) 
+	VALUES (2, 'Magnus', 'Doctorem', 'Lectionarum', '5551123456789', '77722116683');
+INSERT INTO "professores"(fk_pessoa, codigo_matricula) VALUES (2, '166-777-221');
+
 COMMIT;
