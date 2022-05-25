192da8fab961d66974f60141c927665b973df91d
SamuelUniverso <samuel.rockenbach@universo.univates.br>
2022-05-25 00:23:17 -0300

    Database
    #changes to database structure

--- src/main/resources/database/app.database.db.sql
+++ src/main/resources/database/app.database.db.sql
@@ -2,8 +2,8 @@ BEGIN TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS "pessoas" (
 	"id"	INTEGER NOT NULL UNIQUE,
-	"prenome"	TEXT NOT NULL,
-	"nome"	TEXT,
+	"prenome"	TEXT,
+	"nome"	TEXT NOT NULL,
 	"sobrenome"	TEXT NOT NULL,
 	"telefone"	TEXT,
 	"cpf"	TEXT NOT NULL UNIQUE,
@@ -11,14 +11,14 @@ CREATE TABLE IF NOT EXISTS "pessoas" (
 );
 
 CREATE TABLE IF NOT EXISTS "ldap" (
-	  "id" INTEGER NOT NULL UNIQUE,
-	, "usuario" TEXT
-	, "senha" TEXT
-	, "fk_pessoa" INTEGER REFERENCES "pessoas"("id")
+	"id" INTEGER NOT NULL UNIQUE,
+	"usuario" TEXT NOT NULL,
+	"senha" TEXT NOT NULL,
+	"fk_pessoa" INTEGER REFERENCES "pessoas"("id")
 );
 
 CREATE TABLE IF NOT EXISTS "alunos" (
-	"fk_pessoa"	INTEGER UNIQUE,
+	"fk_pessoa"	INTEGER NOT NULL UNIQUE,
 	"codigo_matricula"	INTEGER NOT NULL,
 	FOREIGN KEY("fk_pessoa") REFERENCES "pessoas"("id")
 );
@@ -30,36 +30,36 @@ CREATE TABLE IF NOT EXISTS "professores" (
 );
 
 CREATE TABLE IF NOT EXISTS "cursos" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"nome"	INTEGER NOT NULL,
+	"id" INTEGER NOT NULL UNIQUE,
+	"nome" INTEGER NOT NULL,
 	PRIMARY KEY("id")
 );
 
 CREATE TABLE IF NOT EXISTS "disciplinas" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"nome"	TEXT NOT NULL,
+	"id" INTEGER NOT NULL UNIQUE,
+	"nome" TEXT NOT NULL,
 	PRIMARY KEY("id")
 );
 
 CREATE TABLE IF NOT EXISTS "contratos" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"fk_aluno"	INTEGER NOT NULL,
-	"fk_curso"	INTEGER NOT NULL,
+	"id" INTEGER NOT NULL UNIQUE,
+	"fk_aluno" INTEGER NOT NULL,
+	"fk_curso" INTEGER NOT NULL,
 	"data_ativacao"	TEXT NOT NULL,
-	"data_desativacao"	TEXT,
+	"data_desativacao" TEXT,
 	PRIMARY KEY("id"),
 	FOREIGN KEY("fk_curso") REFERENCES "cursos"("id"),
 	FOREIGN KEY("fk_aluno") REFERENCES "alunos"("id")
 );
 
 CREATE TABLE IF NOT EXISTS "periodos" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"periodo_letivo"	TEXT NOT NULL,
+	"id" INTEGER NOT NULL UNIQUE,
+	"periodo_letivo" TEXT NOT NULL,
 	PRIMARY KEY("id")
 );
 
 CREATE TABLE IF NOT EXISTS "turmas" (
-	"id"	INTEGER NOT NULL UNIQUE,
+	"id" INTEGER NOT NULL UNIQUE,
 	"fk_professor"	INTEGER NOT NULL,
 	"fk_disciplina"	INTEGER NOT NULL,
 	"fk_periodo"	INTEGER NOT NULL,
@@ -85,7 +85,7 @@ CREATE TABLE IF NOT EXISTS "matriculas" (
 );
 
 -- Dummy Data
-INSERT INTO ldap (id, usuario, senha,fk_pessoa) 
+INSERT INTO ldap (id, usuario, senha, fk_pessoa) 
 	VALUES (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', null); -- md5 hash for 'admin'
 
 INSERT INTO "pessoas"(id, prenome, nome, sobrenome, telefone, cpf) 
